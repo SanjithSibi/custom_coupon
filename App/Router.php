bce0bd007b6f20d4c332623603b08fb863106591
SanjithSibi <sanjithmani20@gmail.com>
2023-04-24 16:00:56 +0530

    add custom coupon at cart total field

--- App/Router.php
+++ App/Router.php
@@ -11,13 +11,9 @@ class Router
     public function hooks(){
         $base=new Base();
         add_action('admin_menu', array($base, 'addingMenu'));
-//        add_action('wp_ajax_submitForm', array($base, 'submitForm'));
-//        add_action('wp_ajax_privation_submitForm', array($base, 'submitForm'));
-//        add_action( 'woocommerce_cart_calculate_fees', array($base,'applyCustomCoupon' ));
-        //add_action('woocommerce_before_calculate_totals', array($base,'applyCoupon'));
-//        add_action( 'woocommerce_before_calculate_totals', array($base,'auto_apply_non_persistent_coupon' ));
-//        add_action( 'delete_temp_coupon', array($base,'delete_temporary_coupon' ));
-        add_action('woocommerce_before_cart',array($base, 'cxc_apply_coupon_code_cart'));
+        add_action('wp_ajax_submitForm', array($base, 'submitForm'));
+        add_action( 'woocommerce_cart_calculate_fees', array($base,'addVirtualCoupon' ));
+
 
 
 
